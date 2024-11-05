10591a3f6ee3e4c1b79216f3ee235b37d9d76899
sanmay06 <sanmaykrishnapurachar@gmail.com>
2024-11-05 22:45:03 +0530

    v2.0

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import SlideContext from "../slidecontext";
-
+import { motion } from "framer-motion";
 
 export default function About() {
     const [isVisible, setVisible] = useState(false);
     const { add, remove } = useContext(SlideContext);
-
+    const ref = useRef();
     useEffect(() => {
-        const ele = document.getElementById("welcome-section");
-        if (!ele) return;
+        /*const ele = document.getElementById("welcome-section");
+        if (!ele) return;*/
 
         const observ = new IntersectionObserver(
             ([entry]) => {
@@ -18,12 +18,10 @@ export default function About() {
                 threshold: 0.1,
             }
         );
+        if(ref.current) observ.observe(ref.current);
 
-        observ.observe(ele);
-
-        // Cleanup observer
         return () => {
-            observ.unobserve(ele);
+            if(ref.current) observ.unobserve(ref.current);
         };
     }, []);
 
@@ -36,7 +34,15 @@ export default function About() {
         }
     }, [isVisible]);
 
-    return <section id="welcome-section"> <h1 className="wel-head headings">About Me</h1>
-            <div className="wel-text body">I'm a passionate developer skilled in creating efficient tools and dynamic web applications. I utilize React to build responsive, user-friendly interfaces that enhance web experiences. I thrive on problem-solving and enjoy tackling real-world challenges through clean and effective code. Additionally, I have developed file cleanup tools that optimize disk space by deleting large files and removing empty directories. I'm always eager to learn and grow in the ever-evolving tech landscape.</div>
-        </section>;
+    return <section id="welcome-section" ref={ref}>  
+                {isVisible && (<motion.section className="welcome" 
+                    animate = {{x:0,z:0}}   
+                    initial = {{x:'100vw',z:0}} 
+                    transition = {{ duration:1}}
+                > 
+                    <h1 className="headings">About Me</h1>  
+                    <div className="wel-text body">I'm a passionate developer skilled in creating efficient tools and dynamic web applications. I utilize React to build responsive, user-friendly interfaces that enhance web experiences. I thrive on problem-solving and enjoy tackling real-world challenges through clean and effective code. Additionally, I have developed file cleanup tools that optimize disk space by deleting large files and removing empty directories. I'm always eager to learn and grow in the ever-evolving tech landscape.</div>
+                </motion.section>)}
+                
+            </section>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Bar from "./navbar";
 import { SlideProvider } from "./slidecontext"; // Import the SlideProvider
 import Content from "./content";
+import { motion } from "framer-motion";
 
 function App() {
     return (
         <SlideProvider> {/* Use SlideProvider here */}
             <section>
-                <Bar />
+                <motion.div
+                 initial = {{y:-100, z:1}}
+                 animate = {{y:0, z:1}}
+                 transition = {{delay:1, duration:1}}>
+                    <Bar />
+                </motion.div>
                 <Content />
             </section>
         </SlideProvider>
