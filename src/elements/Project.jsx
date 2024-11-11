21e1ed02d43fa9711cd7e9845aeb680c6951eaa4
sanmay06 <sanmaykrishnapurachar@gmail.com>
2024-11-11 22:39:46 +0530

    v2.3

--- src/elements/Project.jsx
+++ src/elements/Project.jsx
@@ -7,7 +7,7 @@ const dict = [Loan,weather,clean]
 function Project (props) {
     console.log(Loan);
     return <a className="project" href={props.url}>
-            <img src ={dict[props.k - 1]} alt='project image' />
+            <img src ={dict[props.k - 1]} alt='project-image' />
             <h1 className="proj-name">{props.name}</h1>
             <span className="proj-desc">{props.description}</span>
         </a>
