3afe35be756ca4f5f4b7463530cd3de907ec40ad
sanmay06 <sanmaykrishnapurachar@gmail.com>
2024-11-13 22:13:47 +0530

    v2.5

--- src/elements/Project.jsx
+++ src/elements/Project.jsx
@@ -13,18 +13,19 @@ function Project(props) {
 
     useEffect(() => {
         const obs = new IntersectionObserver(
-            ([entry]) => {visible(entry.isIntersecting)}, {
-                root:null,
-                threshold:0.1
+            ([entry]) => visible(entry.isIntersecting), // Proper state update
+            {
+                root: null,
+                threshold: 0.1,
             }
         );
 
-        if(ref.current) obs.observe(ref.current);
+        if (ref.current) obs.observe(ref.current);
 
         return () => {
-            obs.unobserve(ref.current);
-        }
-    },[])
+            if (ref.current) obs.unobserve(ref.current);
+        };
+    }, []);
 
     const variant = {
         animateB: {
@@ -38,49 +39,51 @@ function Project(props) {
             opacity: 0,
         },
         animateA: {
-            x:0,
+            x: 0,
             transition: {
                 delay: props.id * 0.5,
                 duration: 1,
             },
         },
         hiddenA: {
-            x:props.k%2===0?"1000vw":"-100vw",
-        }
+            x: props.k % 2 === 0 ? "1000vw" : "-100vw", // Using strict equality
+        },
     };
 
     return (
-        <div ref = {ref}>
-            {isvis &&(<>
-                <BrowserView>
-                <motion.a
-                    className="project"
-                    href={props.url}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: props.k * 0.5, duration: 1 }}
-                >
-                    <img src={dict[props.k - 1]} alt="project-image" />
-                    <h1 className="proj-name">{props.name}</h1>
-                    <span className="proj-desc">{props.description}</span>
-                </motion.a>
-            </BrowserView>
-            <MobileView>
-                <motion.a
-                    className="project"
-                    href={props.url}
-                    variants={variant}
-                    initial="hiddenA"
-                    animate="animateA"
-                >
-                    <h1 className="proj-name">{props.name}</h1>
-                    <div class='project-cont'>
-                        <img src={dict[props.k - 1]} alt="project-image" />
-                        <span className="proj-desc">{props.description}</span>
-                    </div>
-                </motion.a>
-            </MobileView>
-            </>)}
+        <div ref={ref}>
+            {isvis && (
+                <>
+                    <BrowserView>
+                        <motion.a
+                            className="project"
+                            href={props.url}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: props.k * 0.5, duration: 1 }}
+                        >
+                            <img src={dict[props.k - 1]} alt={`${props.name} project`} />
+                            <h1 className="proj-name">{props.name}</h1>
+                            <span className="proj-desc">{props.description}</span>
+                        </motion.a>
+                    </BrowserView>
+                    <MobileView>
+                        <motion.a
+                            className="project"
+                            href={props.url}
+                            variants={variant}
+                            initial="hiddenA"
+                            animate="animateA"
+                        >
+                            <h1 className="proj-name">{props.name}</h1>
+                            <div className='project-cont'> {/* Corrected attribute */}
+                                <img src={dict[props.k - 1]} alt={`${props.name} project`} />
+                                <span className="proj-desc">{props.description}</span>
+                            </div>
+                        </motion.a>
+                    </MobileView>
+                </>
+            )}
         </div>
     );
 }
