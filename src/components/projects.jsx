d1c4486657e161558f8bdd3dcb6beed32256145b
sanmay06 <sanmaykrishnapurachar@gmail.com>
2024-11-13 17:55:06 +0530

    v2.4

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,48 +1,63 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import SlideContext from "../slidecontext";
 import proj from "../porj";
 import Project from "../elements/Project";
-import { motion } from "framer-motion";
-import { BrowserView,MobileView } from "react-device-detect";
- 
+
 function Projects() {
-    console.log(proj);
-    const [vis, setvis] = useState("false");
+    const ref = useRef();
+    const [vis, setVis] = useState(false);
+    const [reloadTrigger, setReloadTrigger] = useState(0); 
     const { add, remove } = useContext(SlideContext);
 
     useEffect(() => {
-        const ele = document.getElementById("projects");
-        if(!ele) return;
-
         const obs = new IntersectionObserver(
             ([entry]) => {
-                setvis(entry.isIntersecting)
+                if (entry.isIntersecting) {
+                    setVis(true);
+                    setReloadTrigger(prev => prev + 1); 
+                } else {
+                    setVis(false);
+                }
             }, {
                 root: null,
                 threshold: 0.1,
             }
         );
-        obs.observe(ele);
+        if (ref.current) obs.observe(ref.current);
 
-        return ()=>{ obs.unobserve(ele) }
+        return () => {
+            if (ref.current) obs.unobserve(ref.current);
+        };
+    }, []);
 
-    },[])
-
-    useEffect(() =>{
-        if(vis)
+    useEffect(() => {
+        if (vis) {
             add("projects");
-        else    
+        } else {
             remove("projects");
-    },[vis]);
+        }
+    }, [vis]);
 
-    return <section id="projects">
-            <h1 className="headings">Projects</h1> 
-            <div className="projects">
-                {proj.map((p) => {
-                    return <Project name={p.name} description={p.description} lang = {p.language} url = {p.url} k = {p.id}/>
-                })}
-            </div>
+    return (
+        <section id="projects" ref={ref}>
+            {vis && (
+                <>
+                    <h1 className="headings">Projects</h1>
+                    <div className="projects">
+                        {proj.map((p) => (
+                            <Project
+                                k={p.id} 
+                                name={p.name}
+                                description={p.description}
+                                lang={p.language}
+                                url={p.url}
+                            />
+                        ))}
+                    </div>
+                </>
+            )}
         </section>
+    );
 }
 
 export default Projects;
