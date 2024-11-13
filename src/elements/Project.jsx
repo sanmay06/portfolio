b4edd57b4d470864e5151e6c858b2afeadbc985f
sanmay06 <sanmaykrishnapurachar@gmail.com>
2024-11-13 22:12:05 +0530

    v2.5

--- src/elements/Project.jsx
+++ src/elements/Project.jsx
@@ -45,7 +45,7 @@ function Project(props) {
             },
         },
         hiddenA: {
-            x:props.k%2==0?"1000vw":"-100vw",
+            x:props.k%2===0?"1000vw":"-100vw",
         }
     };
 
