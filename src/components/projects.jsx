7db619834e8b869e32d70d95ecf37da3814b8ef0
sanmay06 <sanmaykrishnapurachar@gmail.com>
2024-11-16 00:07:49 +0530

    v2.6

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,9 +1,25 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import proj from "../porj";
-import Project from "../elements/Project";
 import SlideContext from "../slidecontext";
+import Loan from "../pictures/LoanManagemnet.png";
+import weather from "../pictures/weather.png";
+import clean from "../pictures/cleanup.jpeg";
+import { animate, motion } from "framer-motion";
+
+const dict = [Loan, weather, clean];
 
 function Projects() {
+
+    const variant = {
+        hidden: {
+            scale: 1.5,
+            y:20
+        },
+        animate: {
+            scale:1,
+            y:0,
+        }
+    }
 
     const [isVisible, setvis] = useState(false);
     const { add, remove } = useContext(SlideContext);
@@ -31,14 +47,29 @@ function Projects() {
                     <h1 className="proj-head headings">Projects</h1>
                     <div className="projects">
                         {proj.map((p) => (
-                            <Project
-                                key={p.id}
-                                k={p.id} 
-                                name={p.name}
-                                description={p.description}
-                                lang={p.language}
-                                url={p.url}
-                            />
+                            <motion.a
+                            className="project"
+                            href={p.url}
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity : 1 }}
+                            transition={{duration: 1}}
+                        >
+                            <motion.h1 className="proj-name"
+                                variants={variant}    
+                                whileInView='animate'
+                                initial="hidden"
+                                transition={{duration: 1, delay:0.5}}
+                            >{p.name}</motion.h1>
+                                <motion.div className='project-cont'
+                                    variants={variant}    
+                                    whileInView="animate"
+                                    initial="hidden"
+                                    transition={{duration: 1, delay:0.5}}
+                                > 
+                                    <img src={dict[p.id - 1]} alt={`${p.name} project`} />
+                                    <div className="proj-desc">{p.description}</div>
+                                </motion.div>
+                        </motion.a>
                         ))}
                     </div>
         </section>
