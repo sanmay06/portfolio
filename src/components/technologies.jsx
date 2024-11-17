b90d9ec14bc89495373e15795afd9acdd60a641a
sanmay06 <sanmaykrishnapurachar@gmail.com>
2024-11-17 16:26:43 +0530

    v2.7

--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -5,29 +5,76 @@ import { FaCss3Alt } from "react-icons/fa";
 import { FaJava } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import { FaBootstrap } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 function Technology () {
+
+    const variant = {
+        inviewA:{
+            y:[8,0,-10,0,8],
+            transition:{
+                duration:5,
+                repeat: Infinity,
+                repeatType: 'loop',
+            }
+        },
+        inviewB:{
+            y:[10,0,-8,0,10],
+            transition:{
+                duration:4,
+                repeat: Infinity,
+                repeatType: 'loop',
+            }
+        },inviewC:{
+            y:[7,0,-12,0,7],
+            transition:{
+                duration:6,
+                repeat: Infinity,
+                repeatType: 'loop',
+            }
+        },
+
+    }
+
     return <section id='techno'>
         <h1 className="tech-heading headings">Technologies</h1>
         <div className="tech-cont">
-            <div className="tech">
+            <motion.div className="tech"
+                variants={variant}
+                whileInView="inviewA"
+            >
                 <FaReact size={50}/>
-            </div>
-            <div className="tech">
+            </motion.div>
+            <motion.div className="tech"
+                variants={variant}
+                whileInView="inviewB"
+            >
                 <FaHtml5 size={50}/>
-            </div>
-            <div className="tech">
+            </motion.div>
+            <motion.div className="tech"
+                variants={variant}
+                whileInView="inviewC"
+            >
                 <FaCss3Alt size={50}/>
-            </div>
-            <div className="tech">
+            </motion.div>
+            <motion.div className="tech"
+                variants={variant}
+                whileInView="inviewA"
+            >
                 <FaJava size={50}/>
-            </div>
-            <div className="tech">
+            </motion.div>
+            <motion.div className="tech"
+                variants={variant}
+                whileInView="inviewB"
+            >
                 <IoLogoJavascript size={50}/>
-            </div>
-            <div className="tech">
+            </motion.div>
+            <motion.div className="tech"
+                variants={variant}
+                whileInView="inviewC"
+            >
                 <FaBootstrap size={50}/>
-            </div>
+            </motion.div>
         </div>
     </section>
 }
